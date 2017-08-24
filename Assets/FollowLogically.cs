c7fd0ec1a54953339cee3c6e270a3cf7ef7a8ab4
Tadashi Ogura <t-ogura@nii.ac.jp>
2017-08-24 16:34:47 +0900

    First commit by Ogura
    The system can capture only 5 vive trackers motion.
    The system cannot capture vive controllers, HMD and an external tracker motions.

---
+++ Assets/FollowLogically.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowLogically : MonoBehaviour {
+
+	public Transform target;
+
+    public bool test;
+
+	private Transform representative;
+    private Vector3 lastPos;
+
+	// Use this for initialization
+	void Start () {
+		Initialize ();
+	}
+
+	public void Initialize()
+	{
+        //if (target != null) {
+        //	GameObject go = new GameObject (name + "(representative)");
+        //	go.transform.parent = target;
+        //	go.transform.rotation = transform.rotation;
+        //	go.transform.position = transform.position;
+        //	representative = go.transform;
+        //}
+        //      test = false;
+        if (target != null)
+        {
+            transform.parent = target;
+            name = name + "(moved)";
+        }
+	}
+	
+	//// Update is called once per frame
+	//void Update () {
+	//	if (representative != null) {
+	//		transform.position = representative.position;
+	//		transform.rotation = representative.rotation;
+
+ //           if (test)
+ //           {
+ //               if (lastPos != representative.position)
+ //               {
+ //                   Debug.Log("pos change, " + name);
+ //               }
+ //               lastPos = representative.position;
+ //           }
+	//	}
+	//}
+}
---
+++ README.txt
@@ -0,0 +1,49 @@
+Vive IK Demo README
+---------------------------
+
+
+Vive IK Demo is a full body motion capture program using HTC Vive and HTC trackers. 
+
+
+To get the full documentation:
+- Download "Full Body Motion Capture Using Vive Trackers.docx"
+
+To download the executable:
+- Download "vive_ik_test.rar"
+
+To run in Unity and modify the source code:
+- Download this project and open the folder with Unity 5.4 (mine is 5.4.2f2).
+- Double click the scene vive_ik_demo and hit "Play" button.
+
+Special thanks to:
+- RootMotion for their FinalIK. This project took inspiration from it. If you want to use professional multi-joint IK (or any kind of IK) in your game, go check out FinalIK in Unity AssetStore.
+
+
+Copyright: HTC Corporation
+
+
+
+
+Vive IK Demo 中文说明
+-----------------------------
+
+Vive IK Demo是一个使用HTC Vive和HTC追踪器实现的全身动作捕捉程序。
+
+
+
+怎么获得文档：
+- 下载"Vive Tracker全身动作捕捉开发指南.docx"
+
+怎么获得可执行程序：
+- 下载"vive_ik_test.rar"
+
+怎么在Unity中运行及修改代码：
+- 下载本工程，然后用Unity 5.4打开本文件夹（我用的是5.4.2f2）
+- 双击vive_ik_demo场景，然后点击"Play"按钮
+
+特别感谢：
+- RootMotion，本工程借鉴了它们的FinalIK，如果你想在游戏里加上多骨骼IK，去AssetStore里找找FinalIK吧。
+
+
+Copyright: HTC Corporation
+Contact: james_xiong@htc.com
